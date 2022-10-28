4a2e6d21f1f3389747bdb179f267b0461dcc45de
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-28 20:11:00 +0300

    Add files via upload

---
+++ arrayler ve fonksiyonlar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void printArray(int array [],int size){
+	for(int i =0; i<size;i++){
+		cout<<array[i]<<endl;
+	}
+}
+int main(){
+	int a[]={1,2,3,4,5};
+	
+	printArray(a,5);
+	
+	return 0;
+}
