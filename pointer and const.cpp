7b0a165743fbf179b12df30a64792ab416627394
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-28 21:04:48 +0300

    Add files via upload

---
+++ pointer and const.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printArray(const int *ptr1, const int *ptr2){
+	
+	for(;ptr1 != ptr2 ;ptr1++){
+		
+	
+	}
+}
+int main(){
+	int array[]= {10,20,30,40,50,60,70,80,90};
+	
+	
+	printArray(array+2,array+7);
+
+	 
+}
